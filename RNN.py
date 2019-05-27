4edf885eba96f37bf79f35339cc2822c146d9b55
H Yokohama <b1017083@gmail.com>
2019-05-27 15:27:59 +0900

    firt commit

---
+++ RNN.py
@@ -0,0 +1,22 @@
+from keras.datasets import imdb
+from keras.models import Sequential
+from keras.layers import Embedding, SimpleRNN, Dense
+from keras.preprocessing import sequence
+
+max_features = 10000
+max_len = 500
+batch_size = 32
+
+(input_train, y_train), (input_test, y_test) = imdb.load_data(num_words=max_features)
+
+input_train = sequence.pad_sequences(input_train, maxlen=max_len)
+input_test = sequence.pad_sequences(input_test, maxlen=max_len)
+
+model = Sequential()
+model.add(Embedding(max_features, 32))
+model.add(SimpleRNN(32))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+history = model.fit(input_train, y_train, epochs=10,
+                    batch_size=128, validation_split=0.2)
